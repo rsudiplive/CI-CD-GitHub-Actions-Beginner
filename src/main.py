9cf6f2e3bda13c170721f31cf130d5919a2cbbac
Sudip Roy <roy.star97@gmail.com>
2025-01-11 21:23:09 +0530

    simple demo code for CI/CD

---
+++ src/main.py
@@ -0,0 +1,4 @@
+#simple python program to add two nos.
+
+def add(a,b):
+    return a+b
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+from src.main import add
+
+def test_cases():
+    assert add(2,3)==5
+    assert add(3,4)==7
+    assert add(0,0)==0
